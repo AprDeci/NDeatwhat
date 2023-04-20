37e76019de66173f96923b05aff4e8b2b6688505
Wenxuan Zhao <viz@linux.com>
2023-04-21 00:43:12 +0800

    重构项目，分开渲染不同部分防止闪屏，屏幕边缘不折行

--- script.js
+++ script.js
@@ -1,58 +1,164 @@
-var stop = true
-var box = document.querySelector('.box')
-var contain = document.querySelector('.contain')
-var back = document.querySelector('.back')
-var foods = "湘菜大碗饭 木桶饭 摇滚炒鸡 元气煲饭 新疆干脆面 朝鲜冷面 二餐石锅 山西油泼面 重庆小面 麦多馅饼 胡辣汤 肉夹馍 炸鸡排 爱尚麻辣烫 自选餐 鸡公煲"
-var food = foods.split(' ')
-var tests=[]
-var colors=['#99CCFF','#CCFF99','#FFFFCC','#FF99CC','#009933','#FFCC00']
-var eattimess = 0;
-function draw(){ 
-    back.innerHTML=''
-    if(stop){
-        box.innerHTML = '吃什么'
-    }else{
-        contain.innerHTML= food[parseInt(Math.random()*food.length)]
-        box.innerHTML = '选中!'
-}
+(() => {
+  // 创建会在值变更时自动执行回调的引用对象
+  const createRef = (
+    initialValue = undefined,
+    onchangeCallback = undefined
+  ) => {
+    const ref = {
+      current: initialValue,
+      onchange: onchangeCallback,
+    };
 
-for(var i = 0;i<tests.length;i++){
-            test = tests[i]
-            var span = document.createElement('span')
-            span.style.position = 'absolute'
-            span.style.left = test.sx + 'px'
-            span.style.top = test.sy + 'px'
-            span.style.color = test.color
-           // span.style.fontSize = test.size+ 'px'
-            span.style.fontSize = test.size+ 'px'
-            span.style.opacity = 1 - test.age/100
-            test.sy -= test.vx 
-            test.age++;
-            if(test.age>100){
-                tests.splice(i,1)
-            }
-            if(stop){
-                span.innerHTML = contain.innerHTML
-            }
-            else{
-                span.innerHTML = test.str
-            }
-            back.appendChild(span)
-        }
-if(tests.length<50){
-            tests.push({
-                str:food[parseInt(Math.random()*food.length)],
-                //sx:Math.random()*window.innerWidth,
-                sx:Math.random()*window.screen.availWidth-30,
-                //sy:Math.random()*window.innerHeight,
-                sy:Math.random()*document.body.offsetHeight,
-                color:colors[parseInt(Math.random()*colors.length)],
-                size:Math.random()*20+15,               
-                age:Math.random()*100,
-                vx:Math.random()*5
-            })
-           }
-        }
-    
-    
-setInterval(draw,20)
+    return {
+      get current() {
+        return ref.current;
+      },
+      set current(value) {
+        ref.current = value;
+        typeof ref.onchange === "function" && ref.onchange(ref.current);
+      },
+      get onchange() {
+        return ref.onchange;
+      },
+      set onchange(callback) {
+        ref.onchange = callback;
+        callback(ref.current);
+      },
+    };
+  };
+
+  // 更新间隔
+  const UPDATE_INTERVAL = 20; /* 毫秒 */
+
+  // 容器占位符
+  const CONTAINER_PLACEHOLDER = "今天吃什么";
+
+  // 食物列表
+  const FOODS = [
+    "湘菜大碗饭",
+    "木桶饭",
+    "摇滚炒鸡",
+    "元气煲饭",
+    "新疆干脆面",
+    "朝鲜冷面",
+    "二餐石锅",
+    "山西油泼面",
+    "重庆小面",
+    "麦多馅饼",
+    "胡辣汤",
+    "肉夹馍",
+    "炸鸡排",
+    "爱尚麻辣烫",
+    "自选餐",
+    "鸡公煲",
+  ];
+
+  // 背景文字随机颜色列表
+  const BG_TEXT_COLORS = [
+    "#99CCFF",
+    "#CCFF99",
+    "#FFFFCC",
+    "#FF99CC",
+    "#009933",
+    "#FFCC00",
+  ];
+
+  // 获取列表随机项
+  const getRandomItem = (list) => list[Math.floor(Math.random() * list.length)];
+
+  // 随机食物
+  const foodRef = createRef(CONTAINER_PLACEHOLDER);
+  const doShuffleFood = () => {
+    foodRef.current = getRandomItem(FOODS);
+  };
+
+  // 开始/停止随机食物定时器
+  const timerRef = createRef();
+  const toggleShuffleFood = () => {
+    if (timerRef.current) {
+      clearInterval(timerRef.current);
+      timerRef.current = undefined;
+    } else {
+      timerRef.current = setInterval(doShuffleFood, UPDATE_INTERVAL);
+    }
+  };
+
+  // 绑定随机按钮动作
+  const shuffleButtonEl = document.querySelector("#shuffle-button");
+  shuffleButtonEl.onclick = toggleShuffleFood;
+
+  // 绑定计时器变更回调
+  timerRef.onchange = (handle) => {
+    shuffleButtonEl.textContent = handle === undefined ? "吃什么" : "选中!";
+  };
+
+  // 绑定食物变更回调
+  const containerEl = document.querySelector("#container");
+  foodRef.onchange = (food) => {
+    containerEl.textContent = food;
+  };
+
+  // 生成随机背景文字
+  const BACKGROUND_TEXT_CLASS_NAME = "background-text";
+  const spawnBackgroundTextEl = () => {
+    // 随机文本参数
+    const x = Math.random() * window.innerWidth;
+    const fontSize = `${Math.random() * 20 + 15}px`;
+    const color = getRandomItem(BG_TEXT_COLORS);
+    const food = getRandomItem(FOODS);
+    const moveSpeed = Math.random() * 5;
+
+    // 创建 DOM
+    const spanEl = document.createElement("span");
+    spanEl.className = BACKGROUND_TEXT_CLASS_NAME;
+    spanEl.style.left = `${x}px`;
+    spanEl.style.position = "fixed"; // TODO: move to style.css
+    spanEl.style.fontSize = fontSize;
+    spanEl.style.color = color;
+
+    // 初始化 DOM 控制对象
+    const control = {
+      y: Math.random() * (window.innerHeight - spanEl.clientHeight),
+      lifespan: Math.random() * 100,
+      timer: undefined,
+    };
+
+    // 更新元素
+    const updateEl = () => {
+      spanEl.style.top = `${control.y}px`;
+      spanEl.style.opacity = control.lifespan / 100;
+      spanEl.textContent = timerRef.current ? food : foodRef.current;
+    };
+
+    // 初始化并添加元素到页面
+    updateEl();
+    document.body.appendChild(spanEl);
+
+    // 在生命周期内更新动画帧
+    const update = () => {
+      control.y -= moveSpeed;
+      control.lifespan--;
+
+      // 生命周期结束时移除
+      if (control.lifespan <= 0) {
+        clearInterval(control.timer);
+        spanEl.remove();
+
+        return;
+      }
+
+      updateEl();
+    };
+    control.timer = setInterval(update, UPDATE_INTERVAL);
+  };
+
+  // 更新背景文字
+  const backgroundTextSelector = `.${BACKGROUND_TEXT_CLASS_NAME}`;
+  const updateBackgroundText = () => {
+    const count = document.querySelectorAll(backgroundTextSelector).length;
+    for (let i = count; i < 50; i++) {
+      spawnBackgroundTextEl();
+    }
+  };
+  setInterval(updateBackgroundText, UPDATE_INTERVAL);
+})();
